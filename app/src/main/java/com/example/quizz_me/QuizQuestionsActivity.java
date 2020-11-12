304fa235cef04b0e47ef9b2597fd97bc33ec5f35
a1h2med <ahmedshaery81@gmail.com>
2020-11-13 00:16:42 +0200

    Quiz screen addition

---
+++ app/src/main/java/com/example/quizz_me/QuizQuestionsActivity.java
@@ -0,0 +1,14 @@
+package com.example.quizz_me;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuizQuestionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_questions);
+    }
+}
